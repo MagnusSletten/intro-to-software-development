cdabc3d3d5967f3f38829fb016bc793905816f76
Magnus-Elias Sletten <magnus_sletten@outlook.com>
2026-07-02 08:29:36 +0000

    Add ruff linting

--- src/geo_calculator/calculations.py
+++ src/geo_calculator/calculations.py
@@ -5,32 +5,32 @@ def find_average(number_list: list):
     Returns:
         The average value of the numbers
     """
-    sum = 0 
+    sum = 0
 
     for number in number_list:
-        sum += number 
-    return sum/len(number_list)
+        sum += number
+    return sum / len(number_list)
+
 
 def gardners_equation(velocity):
     """Calculate bulk density
     Args:
         velocity: Velocity in m/s
     Returns:
-        bulk density 
+        bulk density
     """
 
-    if velocity< 0:
-        raise(ValueError("Velocity cannot be negative"))
+    if velocity < 0:
+        raise (ValueError("Velocity cannot be negative"))
 
-    return 0.31*velocity**0.25
+    return 0.31 * velocity**0.25
+
 
 def inverse_gardners_equation(density):
 
-    velocity = (density/0.31)**(1/0.25)
-    
-    if density <0:
-        raise(ValueError("Density cannot be negative"))
+    velocity = (density / 0.31) ** (1 / 0.25)
 
+    if density < 0:
+        raise (ValueError("Density cannot be negative"))
 
     return velocity
-
--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,21 +1,30 @@
-from geo_calculator.calculations import find_average, gardners_equation,inverse_gardners_equation
-import pytest 
+import pytest
+
+from geo_calculator.calculations import (
+    find_average,
+    gardners_equation,
+    inverse_gardners_equation,
+)
+
 
 def test_length_of_string() -> None:
     test_string = "python"  # Arrange: set up the inputs
     length = len(test_string)  # Act: run the thing under test
     assert length == 6  # Assert: check the result
 
-def test_find_average_equal_list():
-    number_list = [5,5,5] #arance
-    value = find_average(number_list) #act
 
-    assert value == 5 #assert 
+def test_find_average_equal_list():
+    number_list = [5, 5, 5]  # arance
+    value = find_average(number_list)  # act
+
+    assert value == 5  # assert
+
 
 def test_find_average_odd_numbers():
-    number_list = [2,3] #Arrange
-    value =  find_average(number_list)
+    number_list = [2, 3]  # Arrange
+    value = find_average(number_list)
     assert value == 2.5
+
 
 def test_gardners_equation() -> None:
     velocity = 2000  # m/s
@@ -24,20 +33,18 @@ def test_gardners_equation() -> None:
     # By default, approx considers numbers within a relative tolerance of 1e-6
     assert gardners_equation(velocity) == pytest.approx(expected_density)
 
+
 def test_inverse_gardners_equation() -> None:
     density = 2.0730949  # g/cm3
     expected_velocity = 2000  # m/s
 
     assert inverse_gardners_equation(density) == pytest.approx(expected_velocity)
 
-    assert inverse_gardners_equation(
-        gardners_equation(expected_velocity)
-    ) == pytest.approx(expected_velocity)
+    assert inverse_gardners_equation(gardners_equation(expected_velocity)) == pytest.approx(expected_velocity)
 
-    assert gardners_equation(inverse_gardners_equation(density)) == pytest.approx(
-        density
-    )
-    
+    assert gardners_equation(inverse_gardners_equation(density)) == pytest.approx(density)
+
+
 @pytest.mark.parametrize(
     "velocity, expected_density",
     [
@@ -46,18 +53,17 @@ def test_inverse_gardners_equation() -> None:
         (4000, 2.4653393),
     ],
 )
-def test_gardners_equation_multiple_values(
-    velocity: float, expected_density: float
-) -> None:
+def test_gardners_equation_multiple_values(velocity: float, expected_density: float) -> None:
     assert gardners_equation(velocity) == pytest.approx(expected_density)
+
 
 def test_gardners_equation_negative_velocity() -> None:
     velocity = -1000  # m/s
-    with pytest.raises(ValueError) as e:
+    with pytest.raises(ValueError):
         gardners_equation(velocity)
 
 
 def test_inverse_gardners_equation_negative_density() -> None:
     density = -1000  # g/cm3
-    with pytest.raises(ValueError) as e:
+    with pytest.raises(ValueError):
         inverse_gardners_equation(density)
