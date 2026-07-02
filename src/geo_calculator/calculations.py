dbebe60fe6cbc1c5d561039601a33bfe2a9afff8
Magnus-Elias Sletten <magnus_sletten@outlook.com>
2026-07-02 08:19:50 +0000

    Add inverse gardners equation

--- src/geo_calculator/calculations.py
+++ src/geo_calculator/calculations.py
@@ -1,4 +1,10 @@
 def find_average(number_list: list):
+    """Calculate the average of some numbers
+    Args:
+        numbers: A sequence of numbers
+    Returns:
+        The average value of the numbers
+    """
     sum = 0 
 
     for number in number_list:
@@ -6,4 +12,25 @@ def find_average(number_list: list):
     return sum/len(number_list)
 
 def gardners_equation(velocity):
+    """Calculate bulk density
+    Args:
+        velocity: Velocity in m/s
+    Returns:
+        bulk density 
+    """
+
+    if velocity< 0:
+        raise(ValueError("Velocity cannot be negative"))
+
     return 0.31*velocity**0.25
+
+def inverse_gardners_equation(density):
+
+    velocity = (density/0.31)**(1/0.25)
+    
+    if density <0:
+        raise(ValueError("Density cannot be negative"))
+
+
+    return velocity
+
